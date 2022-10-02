85a433b8910945a7971615fb24513e87d137c518
Jose Hernandez <h2jose@Joses-Mac-mini.local>
2022-10-02 14:05:23 -0400

    netlifycms integration

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,8 +3,8 @@ import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
 
 export const get = () =>
 	rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
+		title: 'SITE_TITLE',
+		description: 'SITE_DESCRIPTION',
 		site: import.meta.env.SITE,
 		items: import.meta.glob('./blog/**/*.{md,mdx}'),
 	});
