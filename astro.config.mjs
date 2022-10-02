5eb28dd155b06a6650f13ed13cf6b3de3df1bb69
Jose Hernandez <h2jose@Joses-Mac-mini.local>
2022-10-02 14:13:17 -0400

    netlifycms integration sitename

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://resilient-dragon-ecfa44.netlify.app/',
 	integrations: [mdx(), sitemap()],
 });
